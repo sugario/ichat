6dcd918f89c4e9b2621895d28d87374da27de826
sugario <tmelva00@fesb.hr>
2018-08-09 09:31:42 +0200

    Register backend

--- src/components/registration/registration.js
+++ src/components/registration/registration.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
+import fbase from '../../config/firebase';
+
 import './registration.css';
 
 class Registration extends Component {
     constructor(props) {
         super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.signup = this.signup.bind(this);
+
+        this.handleInvalidEmail = this.handleInvalidEmail.bind(this);
+        this.handleSignupError = this.handleSignupError.bind(this);
+
+        this.state = {
+            email: '',
+            password: '',
+            passwordRepeat: ''
+        };
+    }
+
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handleInvalidEmail() {
+        console.log('auth/invalid-email');
+    }
+
+    handleSignupError(error) {
+        switch (error.code) {
+            // case 'auth/invalid-email':
+            //     this.handleInvalidEmail();
+            //     break;
+            default:
+                console.log(error);
+        }
+    }
+
+    signup(e) {
+        e.preventDefault();
+
+        if (this.state.password !== this.state.passwordRepeat) {
+            this.handleSignupError({ code: 'passwords-not-maching' });
+            return;
+        }
+
+        fbase.auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then((u) => { })
+            .then((u) => { console.log(u) })
+            .catch((error) => {
+                this.handleInvalidEmail(error);
+            });
     }
 
     render() {
@@ -16,18 +66,24 @@ class Registration extends Component {
                     <p>Please fill in this form to create an account.</p>
                     <hr className='hr-top' />
 
-                    <label htmlFor='email'>Email</label>
-                    <input type='text' placeholder='Enter email' name='email' className='no-radius' required />
+                    <label>Email</label>
+                    <input type='text' placeholder='Enter email' name='email' id='email'
+                        onChange={this.handleChange} value={this.state.email}
+                        className='no-radius' required />
 
-                    <label htmlFor='password'>Password</label>
-                    <input type='password' placeholder='Enter password' name='password' className='no-radius' required />
+                    <label>Password</label>
+                    <input type='password' placeholder='Enter password' name='password' id='password'
+                        onChange={this.handleChange} value={this.state.password}
+                        className='no-radius' required />
 
-                    <label htmlFor='password-repeat'>Repeat Password</label>
-                    <input type='password' placeholder='Repeat password' name='password-repeat' className='no-radius' required />
+                    <label>Repeat Password</label>
+                    <input type='password' placeholder='Repeat password' name='password-repeat' id='password-repeat'
+                        onChange={this.handleChange} value={this.state.passwordRepeat}
+                        className='no-radius' required />
 
                     <hr className='hr-bottom' />
 
-                    <button type='submit' className='blue'>Register</button>
+                    <button type='submit' className='blue' onClick={this.signup}>Register</button>
 
                     <div className='signin center-text'>
                         <p>Already have an account? <Link style={{ color: 'blue' }} to='/home'>Sign in.</Link></p>
