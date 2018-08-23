684fd148219e6eaa1ebd03cd307f21a1b15a71c0
sugario <tmelva00@fesb.hr>
2018-08-24 00:00:31 +0200

    Adding status updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect
-} from 'react-router-dom'
-
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Firebase from './config/firebase';
 import Login from './components/login/login';
 import Home from './components/home/home';
@@ -18,17 +12,36 @@ export default class App extends React.Component {
         this.state = {
             user: null
         }
+
+        this.connectedRef = Firebase.database().ref().child('.info/connected');
+        this.usersRef = Firebase.database().ref().child('users');
+
+        this.updateStatus = this.updateStatus.bind(this);
     }
 
     componentDidMount() {
         this.authListener();
     }
 
+    updateStatus(status) {
+        if (!this.state.user) {
+            return;
+        }
+
+        this.usersRef
+            .orderByChild('email')
+            .equalTo(this.state.user.email)
+            .once('child_added', currentUser => {
+                currentUser.ref.update({ status: status });
+            });
+    }
+
     authListener() {
         Firebase.auth().onAuthStateChanged((user) => {
             if (user) {
-                this.setState({ user });
+                this.setState({ user }, () => this.updateStatus('online'));
             } else {
+                this.updateStatus('offline');
                 this.setState({ user: null });
             }
         });
