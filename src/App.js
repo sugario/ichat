69ee583f25d63826aeb2a66b6a896b23e226a91c
sugario <tmelva00@fesb.hr>
2018-09-01 13:16:57 +0200

    Stopped login rerendering on refresh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Firebase from './config/firebase';
-import Login from './components/login/login';
+
 import Home from './components/home/home';
+import Login from './components/login/login';
 import Registration from './components/registration/registration';
+
 import './App.css';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            user: null
-        }
+            user: sessionStorage.getItem('userCache')
+        };
 
         this.connectedRef = Firebase.database().ref().child('.info/connected');
         this.usersRef = Firebase.database().ref().child('users');
-
         this.updateStatus = this.updateStatus.bind(this);
     }
 
@@ -37,10 +39,12 @@ export default class App extends React.Component {
     }
 
     authListener() {
-        Firebase.auth().onAuthStateChanged((user) => {
+        Firebase.auth().onAuthStateChanged(user => {
             if (user) {
+                sessionStorage.setItem('userCache', this.state.user);
                 this.setState({ user }, () => this.updateStatus('online'));
             } else {
+                sessionStorage.removeItem('userCache');
                 this.updateStatus('offline');
                 this.setState({ user: null });
             }
@@ -52,11 +56,12 @@ export default class App extends React.Component {
             <div className='App'>
                 <Router>
                     <Switch>
-                        <Route exact path='/' render={() => <Redirect to='/home' />} />
+                        <Route exact path='/'
+                               render={() => <Redirect to='/home' />} />
                         <Route path='/home' render={() => {
                             return (
-                                <div>
-                                    {this.state.user ? <div /> : <Login />}
+                                <div className='home'>
+                                    {(!this.state.user) ? <Login /> : <div />}
                                     <Home user={this.state.user} />
                                 </div>
                             );
