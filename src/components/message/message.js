f912e6ffee19236adf4cf0d6b9a4c0dc5a4b8d5c
sugario <tmelva00@fesb.hr>
2018-09-02 15:43:14 +0200

    Message clean-up

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -1,15 +1,16 @@
 import React from 'react';
+
 import './message.css';
 
 export default class extends React.Component {
     render() {
-        let source = 'recieved';
-        if (this.props.message.userName === this.props.currentUser) {
-            source = 'sent';
-        }
+        let type;
+        (this.props.message.userName === this.props.currentUser)
+            ? type = 'sent'
+            : type = 'recieved';
 
         return (
-            <div className={'message ' + source} >
+            <div className={'message ' + type} >
                 <span className='author'>
                     {this.props.message.userName}:
                 </span>
